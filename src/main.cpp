f8d63836c57a86c2db08c516205af5304712ff32
eltoncvlima <eltoncvlima@gmail.com>
2026-09-06 14:34:15 -0300

    Loading the BIOS

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,7 @@
 #include <cstdint>
+#include <fstream>
+#include <iostream>
+#include <vector>
 
 class Cpu {
  public:
@@ -12,5 +15,36 @@ class Cpu {
     this->pc += 4;
 
     // decode_and_execute(instruction);
+  }
+};
+
+constexpr static const uint64_t BIOS_SIZE{512 * 1024};
+
+class Bios {
+ public:
+  std::vector<uint8_t> data;
+
+  Bios(const char* path) {
+    std::ifstream file(path, std::ios::binary);
+
+    std::vector<uint8_t> data(BIOS_SIZE);
+
+    file.read(reinterpret_cast<char*>(data.data()), BIOS_SIZE);
+    file.close();
+
+    if (file.gcount() == BIOS_SIZE) {
+      this->data = data;
+    } else {
+      std::cout << "Invalid_BIOS_size" << std::endl;
+    }
+  }
+
+  uint32_t load32(uint32_t offset) const {
+    const uint32_t b0{static_cast<uint32_t>(data[offset])};
+    const uint32_t b1{static_cast<uint32_t>(data[++offset])};
+    const uint32_t b2{static_cast<uint32_t>(data[++offset])};
+    const uint32_t b3{static_cast<uint32_t>(data[++offset])};
+
+    return b0 | (b1 << 8) | (b2 << 16) | (b3 << 24);
   }
 };
