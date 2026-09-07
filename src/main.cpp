fdaccddbb357b33e037ae77de1f3d9b43624ec02
eltoncvlima <eltoncvlima@gmail.com>
2026-09-06 21:51:39 -0300

    The interconnect

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,14 @@
 #include <cstdint>
 #include <fstream>
 #include <iostream>
+#include <optional>
 #include <vector>
 
 class Cpu {
- public:
+ private:
   uint32_t pc{0xbfc00000};
 
+ public:
   void run_next_instruction() {
     uint32_t pc{this->pc};
 
@@ -21,9 +23,10 @@ class Cpu {
 constexpr static const uint64_t BIOS_SIZE{512 * 1024};
 
 class Bios {
- public:
+ private:
   std::vector<uint8_t> data;
 
+ public:
   Bios(const char* path) {
     std::ifstream file(path, std::ios::binary);
 
@@ -33,7 +36,7 @@ class Bios {
     file.close();
 
     if (file.gcount() == BIOS_SIZE) {
-      this->data = data;
+      this->data = std::move(data);
     } else {
       std::cout << "Invalid_BIOS_size" << std::endl;
     }
@@ -46,5 +49,45 @@ class Bios {
     const uint32_t b3{static_cast<uint32_t>(data[++offset])};
 
     return b0 | (b1 << 8) | (b2 << 16) | (b3 << 24);
+  }
+};
+
+namespace map {
+class Range {
+ public:
+  uint32_t start;
+  uint32_t length;
+
+  Range(const uint32_t start, const uint32_t length)
+  : start{start}, length{length} {}
+
+  std::optional<uint32_t> contains(const uint32_t addr) const {
+    if ((addr >= start) && (addr < (start + length))) {
+      return addr - start;
+    } else {
+      return std::nullopt;
+    }
+  }
+};
+
+const static Range BIOS(0xbfc00000, 512 * 1024);
+};  // namespace map
+
+class Interconnect {
+ public:
+  Bios bios;
+
+  Interconnect(const Bios& bios) : bios{bios} {}
+
+  uint32_t load32(const uint32_t addr) const {
+    const std::optional offset{map::BIOS.contains(addr)};
+
+    if (offset) {
+      return bios.load32(offset.value());
+    } else {
+      std::cout << "unhandled_fetch32_at_address_" << std::hex << addr << std::endl;
+
+      throw -1;
+    }
   }
 };
